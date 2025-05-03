6792204b036b58bbd2a7fd45fc7a140c7f9c4d8e
Angie Li <152510768+angiel22@users.noreply.github.com>
2025-05-03 13:17:59 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -4,6 +4,28 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(() => document.body.style.opacity = 1, 100);
 });
 
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute("id");
+      const link = document.querySelector(`.nav-link[href="#${id}"]`);
+      if (entry.isIntersecting) {
+        navLinks.forEach((link) => link.classList.remove("active"));
+        link.classList.add("active");
+      }
+    });
+  },
+  {
+    threshold: 0.6,
+  }
+);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
 
 function copyEmail() {
     var copyText = "angiel.4work@gmail.com";
